8dd194eaac096e86dfed6d2f3caf557cc80d1611
ychixm <yochix.M@hotmail.fr>
2022-05-20 16:48:52 +0200

    Add filter on script

--- script/json_to_test_dataset.py
+++ script/json_to_test_dataset.py
@@ -5,6 +5,8 @@
 Convert data from json converted pcap to json readable by cpp unit tests
 """
 
+from ast import arg
+from multiprocessing.sharedctypes import Value
 from os import path
 import json
 from docopt import docopt
@@ -14,14 +16,17 @@ doc = f"""
 {__file__}
 
 Usage:
-    {__file__} <input> [--output=file] [--indent]
+    {__file__} <input> [--output=file] [--indent] [--filter=string] [--tags=string]
 
     input               Input file, converted json from pcap using 'pcap_to_json.py'
+    tags                string, can be a list of element "first,second"
 
 Options:
     -h, --help          Show this help message
     -o --output=file    Output file, in which converted data will be stored
     --indent            If set, output will be indented with 4 spaces
+    --filter=string     If the filter is not on the request, the request is ignored
+    --tags=string       tag for the filtered fingerprint 
 """
 
 def parse_data(entry):
@@ -53,16 +58,29 @@ if __name__ == "__main__":
     input = arguments["<input>"]
     output = arguments["--output"]
     indent = arguments["--indent"]
+    filter_value = arguments["--filter"]
+    tags = arguments["--tags"].split(',')
+
 
     with open(path.abspath(input), 'r', encoding='utf8') as f:
         data = json.load(f)
 
         res = [ parse_data(d) for d in data ]
 
+        # find the request with this string inside
+        valid_req_lit = []
+       
+        for req in res:
+            for v in req.values():
+                if filter_value in v:             
+                    valid_req_lit.append(req.copy())
+                    valid_req_lit[-1]["tags"] = tags
+        
+        
         if output:
             with open(path.abspath(output), 'w', encoding='utf8') as out:
-                json.dump(res, out, indent=4 if indent else None)
+                json.dump(valid_req_lit, out, indent=4 if indent else None)
 
             print(f"Done. Result written to {output} ({len(res)} entries)")
         else:
-            print(json.dumps(res, indent=4 if indent else None))
+            print(json.dumps(valid_req_lit, indent=4 if indent else None))
